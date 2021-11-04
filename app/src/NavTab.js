787d5011c1a0e278ba5068f9dddaf63d8868211b
Antoine Bidaud <antoine.bidaud@yahoo.Fr>
2021-11-04 15:40:06 +0100

    Add Icons for navigation

--- app/src/NavTab.js
+++ app/src/NavTab.js
@@ -2,11 +2,15 @@ import * as React from 'react';
 import { Text, View } from 'react-native';
 import { NavigationContainer } from '@react-navigation/native';
 import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { AntDesign } from '@expo/vector-icons'; 
+import { Ionicons } from '@expo/vector-icons';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 
 function OffersScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <AntDesign name="bars" size={24} color="black" />
         <Text>Les offres</Text>
         </View>
     );
@@ -15,14 +19,16 @@ function OffersScreen() {
 function MessageScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <Ionicons name="md-chatbubbles" size={24} color="black" />
         <Text>Messagerie</Text>
         </View>
     );
 }
-
+//Voir pour changer 
 function ProfilScreen() {
     return (
         <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+        <MaterialCommunityIcons name="face-profile" size={24} color="black" />
         <Text>Profil</Text>
         </View>
     );
